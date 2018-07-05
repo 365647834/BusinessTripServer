3607780f39cf5f696e1049131aeb5451315914af
365647834 <dlwuestc@163.com>
2018-07-05 13:05:58 +0800

    杜力为创建工程

---
+++ src/main/java/com/swiet/businesstripserver/BusinesstripserverApplication.java
@@ -0,0 +1,12 @@
+package com.swiet.businesstripserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinesstripserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinesstripserverApplication.class, args);
+    }
+}
