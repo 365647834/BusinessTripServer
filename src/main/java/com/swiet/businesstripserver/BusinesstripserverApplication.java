3a94cf03e4d2e559f3b47eb3fbe7d0a55723ca4c
365647834 <dlwuestc@163.com>
2018-07-05 13:27:56 +0800

    杜力为创建spring-boot

--- src/main/java/com/swiet/businesstripserver/BusinesstripserverApplication.java
+++ src/main/java/com/swiet/businesstripserver/BusinesstripserverApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BusinesstripserverApplication {
+public class BusinesstripserverApplication{
 
     public static void main(String[] args) {
+
         SpringApplication.run(BusinesstripserverApplication.class, args);
     }
 }
+
+
+
---
+++ src/test/java/com/swiet/businesstripserver/BusinesstripserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swiet.businesstripserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BusinesstripserverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
